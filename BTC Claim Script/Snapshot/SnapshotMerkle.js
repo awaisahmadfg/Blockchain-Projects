be3e25a910ce10f351de3d45fe1fa337dd094cc8
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2024-01-18 23:08:24 +0500

    Create SnapshotMerkle.js

---
+++ BTC Claim Script/Snapshot/SnapshotMerkle.js
@@ -0,0 +1,37 @@
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+const fs = require('fs');
+const btcData = require("../scripts/results/Snapshot8Bytes20.json");
+
+// Create leaves from BTC bytes20 addresses and satoshis
+const leaves = btcData.map(data => keccak256(Buffer.concat([
+    Buffer.from(data.bytes20Address.slice(2), 'hex'), 
+    Buffer.from(data.satoshis.toString(16).padStart(64, '0'), 'hex')
+])));
+
+// Create the Merkle tree
+const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+
+// Get the Merkle root
+const root = tree.getRoot().toString('hex');
+
+// Create a proof for a specific dummy address and satoshis
+const specificData = btcData[0];
+const leaf = keccak256(Buffer.concat([
+    Buffer.from(specificData.bytes20Address.slice(2), 'hex'), 
+    Buffer.from(specificData.satoshis.toString(16).padStart(64, '0'), 'hex')
+]));
+
+// To generate a Merkle proof for the selected leaf.
+const proof = tree.getProof(leaf).map(x => '0x' + x.data.toString('hex'));
+
+// Prepare the data to be saved
+const dataToSave = {
+  root: '0x' + root,
+  proof: proof
+};
+
+// Write the data to a file
+fs.writeFileSync('../scripts/results/MerkleProofData.json', JSON.stringify(dataToSave, null, 2));
+
+console.log('Merkle data written to MerkleProofData.json');
