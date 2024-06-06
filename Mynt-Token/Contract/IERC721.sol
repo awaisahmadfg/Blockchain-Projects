fb077691fb7929723224db2d7ed06eef9cb4daa1
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2024-06-06 16:08:20 +0500

    Update IERC721.sol

--- Mynt-Token/Contract/IERC721.sol
+++ Mynt-Token/Contract/IERC721.sol
@@ -1,23 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
+
+import "./IERC165.sol";
 
 // ============ Interfaces ============
-interface IERC721  {
-
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-
+interface IERC721 is IERC165 {
     function ownerOf(uint256 tokenId) external view returns (address owner);
-    function _burn(uint256 tokenId) external;
-    function burn(uint256 tokenId) external;
-
-    function balanceOf(address owner) external view returns (uint256 balance);
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
-    function safeTransferFrom(address from, address to, uint256 tokenId) external;
-    function transferFrom(address from, address to, uint256 tokenId) external;
-    function approve(address to, uint256 tokenId) external;
-    function setApprovalForAll(address operator, bool approved) external;
-    function getApproved(uint256 tokenId) external view returns (address operator);
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
 }
