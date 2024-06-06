acaba894cf793994329e1443b7d8f39bf368f1c3
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2024-06-06 16:08:44 +0500

    Create IERC165.sol

---
+++ Mynt-Token/Contract/IERC165.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// ============ Interfaces ============
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
