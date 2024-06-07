f293dc28dfec86c09003e4cf0695dacc337cc02a
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2024-06-07 22:22:37 +0500

    Update IFMYNT.sol

--- Mynt-Token/Contract/IFMYNT.sol
+++ Mynt-Token/Contract/IFMYNT.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.20;
 
 // ============ Interfaces ============
 interface IFMYNT{
-    function mint(address _receiver, uint256 _amount, address _contractAddress) external;
-    function treasureBoxMintedAmount() external view returns (uint256);
-    function TREASURE_BOX_SUPPLY_CAP() external view returns (uint256);
-    function treasureBoxAddress() external view returns (address);
+    // function mint(address _receiver, uint256 _amount, address _contractAddress) external;
+    // function treasureBoxMintedAmount() external view returns (uint256);
+    // function TREASURE_BOX_SUPPLY_CAP() external view returns (uint256);
+    // function treasureBoxAddress() external view returns (address);
+    function TREASURE_BOX() external view returns (address);
+    function transferReward(uint256 _amount) external;
+    function getTreasureBoxAssests() external view returns (uint256);
 }
