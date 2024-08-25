8eae2b2fe5817fa5105e4beaf62617ccb0162eeb
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2024-08-25 17:28:22 +0500

    Update UploadImageUrlToIpfs.js

--- Mind-Miner/UploadImageUrlToIpfs.js
+++ Mind-Miner/UploadImageUrlToIpfs.js
@@ -1,1 +1,26 @@
+const axios = require('axios');
 
+
+  async uploadImageUrlToIpfs(imageUrl) {
+    try {
+      const response = await axios.get(imageUrl, { responseType: 'stream' });
+      const urlParts = imageUrl.split('/');
+      const defaultFileName = urlParts[urlParts.length - 1] || 'image_from_url';
+
+      const options = {
+        pinataMetadata: {
+          name: defaultFileName,
+        },
+        pinataOptions: {
+          cidVersion: 0,
+        },
+      };
+
+      const ipfs = await this.pinata.pinFileToIPFS(response.data, options);
+
+      return [`${PINATA_URL}/ipfs/${ipfs.IpfsHash}`];
+    } catch (error) {
+      const message = error.message || error.details;
+      throw new Error(`Image upload error: ${message}`);
+    }
+  }
