1ade22890268350dbba54f8478903ed888b11a86
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2024-11-03 23:49:17 +0500

    Update SideBysideWay.tsx

--- Mind-Miner/SideBysideWay.tsx
+++ Mind-Miner/SideBysideWay.tsx
@@ -1,1 +1,6 @@
-
+        <Box sx={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+          <StyledTitle>{concept.title}</StyledTitle>
+          <IconButton edge="end" >
+            <MoreVertIcon sx={{ fontSize: 30 }} />
+          </IconButton>
+        </Box>
