41d412948475c9830b6ffeb2d2c142d96c4e51eb
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2025-02-09 03:04:28 +0500

    Create selector.tsx

---
+++ Mind-Miner/selector.tsx
@@ -0,0 +1,6 @@
+export const GetLocationTagsLoading = () =>
+  useSelector((state: any) => state.home.locationTagsLoading);
+export const GetWorkplaceTagsLoading = () =>
+  useSelector((state: any) => state.home.workplaceTagsLoading);
+export const GetUniversityTagsLoading = () =>
+  useSelector((state: any) => state.home.universityTagsLoading);
