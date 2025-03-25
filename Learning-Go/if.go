a7d1d2182cdd64871351d21a6d91bdde2093cb2b
awaisahmadfg <awaisahmadfg@gmail.com>
2025-03-26 00:13:04 +0500

    checking git config

---
+++ Learning-Go/if.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if 8%2 == 0 || 7%2 == 0 {
+		fmt.Println("either 8 or 7 are even")
+	}
+	// Thanks
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
+
+/*
+	There is no ternary if in Go, so you’ll need to use a full if statement even for basic conditions.
+*/
